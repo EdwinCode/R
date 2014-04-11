7673145aeaa398a757e0d5c3bbb48f4a6dbb3469
Edwin Vehmaanperä <mardeth@gmail.com>
2014-04-12 00:50:08 +0300

    Improvement 2

--- joo.R
+++ joo.R
@@ -1,9 +1,19 @@
-pollutantmean <- function(directory, pollutant, id = 1:332) {
+pollutantmean <- function(directory=getwd(), pollutant="sulfate", id = 1:332) {
   setwd(directory)
   dataFiles <- lapply(Sys.glob("*.csv"), read.csv)
   
   sulf<-pollutant=="sulfate"
+  data<-c(NA)
   
   if(sulf==T){
-    
+    for(i in id){
+      data<-c(data,dataFiles[[i]][,2])
+    }
+      
+    }else{
+      for(i in id){
+        data<-c(data,dataFiles[[i]][,3])
+      }
+    }
+    mean(data, na.rm=TRUE)
   }

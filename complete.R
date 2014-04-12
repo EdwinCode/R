6502c30bf499e8aa16b47b75425773aa384f15a9
Edwin Vehmaanperä <mardeth@gmail.com>
2014-04-12 17:57:08 +0300

    Final / Worrking

--- complete.R
+++ complete.R
@@ -1,2 +1,16 @@
-complete <- function(directory=getwd(), id = 1:332) {
+complete <- function(directory="specdata", id = 1:332) {
   setwd(paste("C:/Users/Edwin/Documents/", directory, sep=""))
+  dataFiles <- lapply(Sys.glob("*.csv"), read.csv)
+  
+  df<-data.frame(id=numeric(),nobs=numeric())
+  x<-0
+  
+  for(i in id){
+    data<-data.frame(dataFiles[[i]][,2:3])
+    data<-na.omit(data)
+    row<-c(i,nrow(data))
+    df<-rbind(df,row)
+  }
+  names(df)<-c("id","nobs")
+  df
+}
